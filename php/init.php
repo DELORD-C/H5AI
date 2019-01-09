ff17922081eac6796f95539033eec093b449dc55
DELORD Clément <clement.delord@epitech.eu>
2019-01-09 11:39:01 +0100

    Universal path

--- php/init.php
+++ php/init.php
@@ -5,7 +5,7 @@ include 'php/class/explorer_class.php';
 include 'php/class/navpanel_class.php';
 
 $url = str_replace('%20', ' ', rtrim(ltrim($_SERVER['REQUEST_URI'], '/'), '/'));
-$path = 'C:/Users/Zulgard/AppData/Local/Packages/CanonicalGroupLimited.Ubuntu18.04onWindows_79rhkp1fndgsc/LocalState/rootfs/home/zulgard/www/' . $url;
+$path = substr(getcwd(), 0, -7) . $url;
 $dirs = scandir($path);
 $opened = explode('/', $url);
 
